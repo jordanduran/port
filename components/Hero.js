dba5d7ccaf4a25a212d8096d861ab7afc3b01baa
Jordan Duran <jordanduran@Jordans-MacBook-Pro.local>
2021-10-09 19:21:34 -0400

    Hero and Navbar component

---
+++ components/Hero.js
@@ -0,0 +1,25 @@
+const Hero = () => {
+  return (
+    <div className='hero-container'>
+      <h1 className='hero-header text-blue-700 text-5xl font-bold'>
+        Hi, I&apos;m Jordan
+      </h1>
+      <p className='hero-bio max-w-prose mt-6'>
+        A New York City based front-end developer with a passion for building
+        excellent software with exquisite user interfaces.
+      </p>
+      <p className='hero-bio max-w-prose mt-6'>
+        With several years of experience in the tech industry, I have
+        specialized in creating software for clients ranging from individuals
+        and small-businesses all the way to large enterprise corporations.
+      </p>
+      <p className='hero-bio max-w-prose mt-6'>
+        I pride myself in being a hardworking, self-disciplined person who is
+        also very easy to work with. When I&apos;m not building software, I like
+        to spend time playing video games, skateboarding, or snowboarding.
+      </p>
+    </div>
+  );
+};
+
+export default Hero;
