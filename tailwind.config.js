bd76f0e3ca14f5e1a4e65cdb2357222e1f8813b3
Jordan Duran <jordanduran@Jordans-MacBook-Pro.local>
2021-10-09 14:12:37 -0400

    Tailwind install and setup

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./pages/index.js'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
