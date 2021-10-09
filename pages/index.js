c5db70efd2bcb5e683576ba0f61985846dd02c48
Jordan Duran <jordanduran@Jordans-MacBook-Pro.local>
2021-10-09 19:21:06 -0400

    Container and App setup

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import Hero from '../components/Hero';
 
 const HomePage = () => {
   return (
-    <div>
-      <h1>Homepage</h1>
+    <div className='max-w-8xl mx-auto px-4 py-10 sm:px-6 lg:px-8'>
+      <div className='max-w-6xl py-8 mx-auto'>
+        <Hero />
+      </div>
     </div>
   );
 };
