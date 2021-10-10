166bbd86a5dc96204f02b924e439722f944c0845
Jordan Duran <jordanduran@Jordans-MacBook-Pro.local>
2021-10-09 23:38:44 -0400

    Footer Component and setup

---
+++ components/Footer.js
@@ -0,0 +1,57 @@
+import {
+  faInstagram,
+  faGithub,
+  faLinkedinIn,
+} from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+const Footer = () => {
+  const navigation = [
+    {
+      name: 'LinkedIn',
+      href: 'https://www.linkedin.com/in/jordan-duran-m/',
+      target: '_blank',
+      icon: () => <FontAwesomeIcon icon={faLinkedinIn} />,
+    },
+
+    {
+      name: 'GitHub',
+      href: 'https://github.com/jordanduran/',
+      target: '_blank',
+      icon: () => <FontAwesomeIcon icon={faGithub} />,
+    },
+    {
+      name: 'Instagram',
+      href: 'https://www.instagram.com/jordan_duran/',
+      target: '_blank',
+      icon: () => <FontAwesomeIcon icon={faInstagram} />,
+    },
+  ];
+
+  return (
+    <footer className='bg-blue-700'>
+      <div className='max-w-7xl mx-auto py-4 px-4 sm:px-6 md:flex md:items-center md:justify-between lg:px-8'>
+        <div className='flex justify-center space-x-6 md:order-2'>
+          {navigation.map((item) => (
+            <a
+              key={item.name}
+              href={item.href}
+              target={item.target}
+              className='footer-social-links text-white hover:text-yellow-200 fa-lg'
+            >
+              <span className='sr-only'>{item.name}</span>
+              <item.icon className='h-6 w-6' aria-hidden='true' />
+            </a>
+          ))}
+        </div>
+        <div className='mt-8 md:mt-0 md:order-1'>
+          <p className='text-center text-base text-white'>
+            &copy; Jordan Duran - 2021
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,14 @@
 import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
 import '../styles/globals.css';
 
 const MyApp = ({ Component, pageProps }) => {
   return (
-    <>
+    <div className='flex flex-col justify-between h-screen'>
       <Navbar />
       <Component {...pageProps} />
-    </>
+      <Footer />
+    </div>
   );
 };
 
