aab23b2f4b8300360dbe8dd9f6e3b055303f6e89
Jordan Duran <jordanduran@Jordans-MacBook-Pro.local>
2021-10-10 01:13:58 -0400

    Removed comment from Navbar and added line-height to Hero bio

--- components/Hero.js
+++ components/Hero.js
@@ -4,17 +4,17 @@ const Hero = () => {
       <h1 className='hero-header text-blue-700 text-5xl font-bold'>
         Hi, I&apos;m Jordan
       </h1>
-      <p className='hero-bio max-w-prose mt-6'>
+      <p className='hero-bio max-w-prose mt-6 leading-loose'>
         A New York City based full-stack developer with a front-end focus that
         has a passion for building excellent software with exquisite user
         interfaces.
       </p>
-      <p className='hero-bio max-w-prose mt-6'>
+      <p className='hero-bio max-w-prose mt-6 leading-loose'>
         With several years of experience in the tech industry, I have
         specialized in creating software for clients ranging from individuals
         and small-businesses all the way to large enterprise corporations.
       </p>
-      <p className='hero-bio max-w-prose mt-6'>
+      <p className='hero-bio max-w-prose mt-6 leading-loose'>
         I pride myself in being a hardworking, self-disciplined person who is
         also very easy to work with. When I&apos;m not building software, I like
         to spend time playing video games, skateboarding, or snowboarding.
--- components/Navbar.js
+++ components/Navbar.js
@@ -15,7 +15,6 @@ const Navbar = () => {
                   </h1>
                 </div>
                 <div className='hidden sm:ml-6 sm:flex sm:space-x-8'>
-                  {/* Current: "border-indigo-500 text-gray-900", Default: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700" */}
                   <a
                     href='#'
                     className='border-indigo-500 text-gray-900 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium'
@@ -52,7 +51,6 @@ const Navbar = () => {
 
           <Disclosure.Panel className='sm:hidden'>
             <div className='pt-2 pb-3 space-y-1'>
-              {/* Current: "bg-indigo-50 border-indigo-500 text-indigo-700", Default: "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700" */}
               <a
                 href='#'
                 className='bg-indigo-50 border-indigo-500 text-indigo-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium'
