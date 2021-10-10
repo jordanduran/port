562e3d90129f7b41602f824ed5d8a7090ced6d6f
Jordan Duran <jordanduran@Jordans-MacBook-Pro.local>
2021-10-09 23:40:14 -0400

    HomePage container revamp

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Hero from '../components/Hero';
 
 const HomePage = () => {
   return (
-    <div className='max-w-8xl mx-auto px-4 py-10 sm:px-6 lg:px-8'>
+    <div className='max-w-8xl px-4 py-8 sm:px-6 lg:px-8'>
       <div className='max-w-6xl py-8 mx-auto'>
         <Hero />
       </div>
