d3d33ca0f48a68c27c49fcf9efaf399c923fb563
Jordan Duran <jordanduran@Jordans-MacBook-Pro.local>
2021-10-11 14:35:45 -0400

    Navbar Links with scroll

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,5 +1,6 @@
 import { Disclosure } from '@headlessui/react';
 import { MenuIcon, XIcon } from '@heroicons/react/outline';
+import { Link, animateScroll as scroll } from 'react-scroll';
 
 const Navbar = () => {
   return (
@@ -15,29 +16,44 @@ const Navbar = () => {
                   </h1>
                 </div>
                 <div className='hidden sm:ml-6 sm:flex sm:space-x-8'>
-                  <a
-                    href='#'
-                    className='border-indigo-500 text-gray-900 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium'
+                  <Link
+                    className='border-transparent text-gray-700 hover:border-gray-300 hover:text-gray-900 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium cursor-pointer'
+                    activeClass='active'
+                    to='hero-section'
+                    spy={true}
+                    smooth={true}
+                    offset={-70}
+                    duration={500}
                   >
                     Home
-                  </a>
-                  <a
-                    href='#'
-                    className='border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium'
+                  </Link>
+                  <Link
+                    className='border-transparent text-gray-700 hover:border-gray-300 hover:text-gray-900 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium cursor-pointer'
+                    activeClass='active'
+                    to='projects-section'
+                    spy={true}
+                    smooth={true}
+                    offset={-70}
+                    duration={500}
                   >
                     Projects
-                  </a>
-                  <a
-                    href='#'
-                    className='border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium'
+                  </Link>
+                  <Link
+                    className='border-transparent text-gray-700 hover:border-gray-300 hover:text-gray-900 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium cursor-pointer'
+                    activeClass='active'
+                    to='blog-section'
+                    spy={true}
+                    smooth={true}
+                    offset={-70}
+                    duration={500}
                   >
                     Blog
-                  </a>
+                  </Link>
                 </div>
               </div>
               <div className='-mr-2 flex items-center sm:hidden'>
                 {/* Mobile menu button */}
-                <Disclosure.Button className='inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500'>
+                <Disclosure.Button className='inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-blue-500'>
                   <span className='sr-only'>Open main menu</span>
                   {open ? (
                     <XIcon className='block h-6 w-6' aria-hidden='true' />
@@ -51,24 +67,39 @@ const Navbar = () => {
 
           <Disclosure.Panel className='sm:hidden'>
             <div className='pt-2 pb-3 space-y-1'>
-              <a
-                href='#'
-                className='bg-indigo-50 border-indigo-500 text-indigo-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium'
+              <Link
+                className='border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium'
+                activeClass='active'
+                to='hero-section'
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={500}
               >
                 Home
-              </a>
-              <a
-                href='#'
+              </Link>
+              <Link
                 className='border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium'
+                activeClass='active'
+                to='projects-section'
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={500}
               >
                 Projects
-              </a>
-              <a
-                href='#'
+              </Link>
+              <Link
                 className='border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium'
+                activeClass='active'
+                to='blog-section'
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={500}
               >
                 Blog
-              </a>
+              </Link>
             </div>
           </Disclosure.Panel>
         </>
