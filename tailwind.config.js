e6e01f8b9b6ca2ba2f5215d428897da3d32ce721
Jordan Duran <jordanduran@Jordans-MacBook-Pro.local>
2021-10-11 13:08:53 -0400

    Tailwind config purge files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,12 @@
 module.exports = {
-  purge: ['./pages/index.js'],
+  purge: [
+    './pages/index.js',
+    '/components/Navbar.js',
+    '/components/Hero.js',
+    '/components/ProjectsSection.js',
+    '/components/Footer.js',
+    '/components/BlogSection.js',
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
