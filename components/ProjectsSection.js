0856fd39f96068fc78519be797722dce7d199bbc
Jordan Duran <jordanduran@Jordans-MacBook-Pro.local>
2021-10-11 00:35:05 -0400

    Renamed component to ProjectSection and changed import

---
+++ components/ProjectsSection.js
@@ -0,0 +1,202 @@
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+const ProjectsSection = () => {
+  return (
+    <div className='project-section mt-10'>
+      <h1 className='projects-header text-4xl font-bold'>Projects</h1>
+      <p className='text-base text-gray-700 mt-1 mb-6'>
+        My set of skills include JavaScript, React, Next.js, CSS, and styling
+        frameworks.
+      </p>
+      <ul
+        role='list'
+        className='grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-2'
+      >
+        {/* Start of Project 1 */}
+        <div className='project-one mb-10'>
+          <span className='project-category text-blue-700 text-sm font-bold uppercase'>
+            Web Application
+          </span>
+          <h2 className='project-title text-2xl font-bold mb-2'>
+            Scenic Forests
+          </h2>
+          <p className='text-base text-gray-600 mb-3'>
+            (HTML5, CSS3, & JavaScript)
+          </p>
+          <li className='project-list list-inside col-span-1 text-gray-800 leading-loose w-100 max-w-lg'>
+            Built for people who want to take a nice vacation away from the
+            city.
+          </li>
+          <li className='project-list list-inside col-span-1 text-gray-800 leading-loose max-w-lg'>
+            Displays wonderful cabin homes in different areas along with high
+            quality amenities.
+          </li>
+          <li className='project-list list-inside col-span-1 text-gray-800 leading-loose max-w-lg'>
+            Multi page layout with eye-catching{' '}
+            <strong>CSS animation and transitions.</strong>
+          </li>
+          <li className='project-list list-inside col-span-1 text-gray-800 leading-loose max-w-lg mb-3'>
+            Fully responsive with amazing typography and flow.
+          </li>
+          <div className='flex justify-evenly'>
+            <button
+              type='button'
+              className='inline-flex items-center px-5 py-1 border border-transparent text-lg font-semibold rounded-md shadow-sm text-white bg-gray-700 hover:bg-gray-800 focus:outline-none'
+            >
+              <a
+                href='https://github.com/jordanduran/Scenic-Forests'
+                rel='noreferrer'
+                target='_blank'
+              >
+                GitHub{' '}
+                <FontAwesomeIcon className='fa-xl ml-2' icon={faGithub} />
+              </a>
+            </button>
+            <button
+              type='button'
+              className='inline-flex items-center px-5 py-1 border border-transparent text-lg font-semibold rounded-md shadow-sm text-white bg-blue-700 hover:bg-blue-800 focus:outline-none'
+            >
+              <a
+                href='https://scenicforests.netlify.app/'
+                rel='noreferrer'
+                target='_blank'
+              >
+                Live View{' '}
+                <FontAwesomeIcon
+                  className='fa-xl ml-2'
+                  icon={faExternalLinkAlt}
+                />
+              </a>
+            </button>
+          </div>
+        </div>
+        {/* End of Project */}
+
+        {/* Start of Project 2 */}
+        <div className='project-two mb-10'>
+          <span className='project-category text-blue-700 text-sm font-bold uppercase'>
+            Web Application
+          </span>
+          <h2 className='project-title text-2xl font-bold mb-2'>
+            Scenic Forests
+          </h2>
+          <p className='text-base text-gray-600 mb-3'>
+            (HTML5, CSS3, & JavaScript)
+          </p>
+          <li className='project-list list-inside col-span-1 text-gray-800 leading-loose w-100 max-w-lg'>
+            Built for people who want to take a nice vacation away from the
+            city.
+          </li>
+          <li className='project-list list-inside col-span-1 text-gray-800 leading-loose max-w-lg'>
+            Displays wonderful cabin homes in different areas along with high
+            quality amenities.
+          </li>
+          <li className='project-list list-inside col-span-1 text-gray-800 leading-loose max-w-lg'>
+            Multi page layout with eye-catching{' '}
+            <strong>CSS animation and transitions.</strong>
+          </li>
+          <li className='project-list list-inside col-span-1 text-gray-800 leading-loose max-w-lg mb-3'>
+            Fully responsive with amazing typography and flow.
+          </li>
+          <div className='flex justify-evenly'>
+            <button
+              type='button'
+              className='inline-flex items-center px-5 py-1 border border-transparent text-lg font-semibold rounded-md shadow-sm text-white bg-gray-700 hover:bg-gray-800 focus:outline-none'
+            >
+              <a
+                href='https://github.com/jordanduran/Scenic-Forests'
+                rel='noreferrer'
+                target='_blank'
+              >
+                GitHub{' '}
+                <FontAwesomeIcon className='fa-xl ml-2' icon={faGithub} />
+              </a>
+            </button>
+            <button
+              type='button'
+              className='inline-flex items-center px-5 py-1 border border-transparent text-lg font-semibold rounded-md shadow-sm text-white bg-blue-700 hover:bg-blue-800 focus:outline-none'
+            >
+              <a
+                href='https://scenicforests.netlify.app/'
+                rel='noreferrer'
+                target='_blank'
+              >
+                Live View{' '}
+                <FontAwesomeIcon
+                  className='fa-xl ml-2'
+                  icon={faExternalLinkAlt}
+                />
+              </a>
+            </button>
+          </div>
+        </div>
+
+        {/* End of Project */}
+
+        {/* Start of Project 3 */}
+        <div className='project-three mb-10'>
+          <span className='project-category text-blue-700 text-sm font-bold uppercase'>
+            Web Application
+          </span>
+          <h2 className='project-title text-2xl font-bold mb-2'>
+            Scenic Forests
+          </h2>
+          <p className='text-base text-gray-600 mb-3'>
+            (HTML5, CSS3, & JavaScript)
+          </p>
+          <li className='project-list list-inside col-span-1 text-gray-800 leading-loose w-100 max-w-lg'>
+            Built for people who want to take a nice vacation away from the
+            city.
+          </li>
+          <li className='project-list list-inside col-span-1 text-gray-800 leading-loose max-w-lg'>
+            Displays wonderful cabin homes in different areas along with high
+            quality amenities.
+          </li>
+          <li className='project-list list-inside col-span-1 text-gray-800 leading-loose max-w-lg'>
+            Multi page layout with eye-catching{' '}
+            <strong>CSS animation and transitions.</strong>
+          </li>
+          <li className='project-list list-inside col-span-1 text-gray-800 leading-loose max-w-lg mb-3'>
+            Fully responsive with amazing typography and flow.
+          </li>
+          <div className='flex justify-evenly'>
+            <button
+              type='button'
+              className='inline-flex items-center px-5 py-1 border border-transparent text-lg font-semibold rounded-md shadow-sm text-white bg-gray-700 hover:bg-gray-800 focus:outline-none'
+            >
+              <a
+                href='https://github.com/jordanduran/Scenic-Forests'
+                rel='noreferrer'
+                target='_blank'
+              >
+                GitHub{' '}
+                <FontAwesomeIcon className='fa-xl ml-2' icon={faGithub} />
+              </a>
+            </button>
+            <button
+              type='button'
+              className='inline-flex items-center px-5 py-1 border border-transparent text-lg font-semibold rounded-md shadow-sm text-white bg-blue-700 hover:bg-blue-800 focus:outline-none'
+            >
+              <a
+                href='https://scenicforests.netlify.app/'
+                rel='noreferrer'
+                target='_blank'
+              >
+                Live View{' '}
+                <FontAwesomeIcon
+                  className='fa-xl ml-2'
+                  icon={faExternalLinkAlt}
+                />
+              </a>
+            </button>
+          </div>
+        </div>
+        {/* End of Project */}
+      </ul>
+    </div>
+  );
+};
+
+export default ProjectsSection;
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
 import Hero from '../components/Hero';
+import ProjectsSection from '../components/ProjectsSection';
 
 const HomePage = () => {
   return (
     <div className='max-w-8xl px-4 py-8 sm:px-6 lg:px-8'>
-      <div className='max-w-6xl py-8 mx-auto'>
+      <div className='hero-section max-w-6xl py-8 mx-auto'>
         <Hero />
+      </div>
+      <div className='project-section max-w-6xl py-8 mx-auto'>
+        <ProjectsSection />
       </div>
     </div>
   );
