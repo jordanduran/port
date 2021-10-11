24c2c350e72df7ddf7251e84840cc974617ad2fd
Jordan Duran <jordanduran@Jordans-MacBook-Pro.local>
2021-10-11 14:36:55 -0400

    BlogSection component

--- components/BlogSection.js
+++ components/BlogSection.js
@@ -1,7 +1,114 @@
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+const posts = [
+  {
+    title: 'Boost your conversion rate',
+    href: '#',
+    category: { name: 'Article', href: '#' },
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto accusantium praesentium eius, ut atque fuga culpa, similique sequi cum eos quis dolorum.',
+    date: 'Mar 16, 2020',
+    datetime: '2020-03-16',
+    imageUrl:
+      'https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80',
+    readingTime: '6 min',
+    author: {
+      name: 'Roel Aufderehar',
+      href: '#',
+      imageUrl:
+        'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    },
+  },
+  {
+    title: 'How to use search engine optimization to drive sales',
+    href: '#',
+    category: { name: 'Article', href: '#' },
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit facilis asperiores porro quaerat doloribus, eveniet dolore. Adipisci tempora aut inventore optio animi., tempore temporibus quo laudantium.',
+    date: 'Mar 10, 2020',
+    datetime: '2020-03-10',
+    imageUrl:
+      'https://images.unsplash.com/photo-1547586696-ea22b4d4235d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80',
+    readingTime: '4 min',
+    author: {
+      name: 'Brenna Goyette',
+      href: '#',
+      imageUrl:
+        'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    },
+  },
+  {
+    title: 'Improve your customer experience',
+    href: '#',
+    category: { name: 'Article', href: '#' },
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint harum rerum voluptatem quo recusandae magni placeat saepe molestiae, sed excepturi cumque corporis perferendis hic.',
+    date: 'Feb 12, 2020',
+    datetime: '2020-02-12',
+    imageUrl:
+      'https://images.unsplash.com/photo-1492724441997-5dc865305da7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80',
+    readingTime: '11 min',
+    author: {
+      name: 'Daniela Metz',
+      href: '#',
+      imageUrl:
+        'https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    },
+  },
+];
+
 const BlogSection = () => {
   return (
-    <div>
-      <h1 className='projects-header text-3xl font-bold'>Recent Blog Posts</h1>
+    <div className='mb-8' id={'blog-section'}>
+      <div className='bg-white h-1/3 sm:h-2/3' />
+
+      <div className='relative max-w-7xl mx-auto'>
+        <div className=''>
+          <h2 className='lg:text-2xl tracking-tight font-extrabold text-gray-900 text-2xl'>
+            Recent Blog Posts
+          </h2>
+        </div>
+        <div className='mt-4 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none'>
+          {posts.map((post) => (
+            <div
+              key={post.title}
+              className='blog-card flex flex-col rounded-lg shadow-lg overflow-hidden'
+            >
+              <div className='flex-shrink-0'>
+                <img
+                  className='h-48 w-full object-cover'
+                  src={post.imageUrl}
+                  alt=''
+                />
+              </div>
+              <div className='card-content flex-1 bg-white p-6 flex flex-col justify-between'>
+                <div className='flex-1'>
+                  <p className='text-sm font-medium text-blue-700'>
+                    <a href={post.category.href} className='hover:underline'>
+                      {post.category.name}
+                    </a>
+                  </p>
+                  <a href={post.href} className='block mt-2'>
+                    <p className='text-xl font-semibold text-gray-900'>
+                      {post.title}
+                    </p>
+                    <p className='card-description mt-3 text-base text-gray-500'>
+                      {post.description}
+                    </p>
+                  </a>
+                  <a href={post.href} className='block mt-2'>
+                    <p className='card-description mt-3 text-base text-blue-700 hover:text-blue-900'>
+                      Read more...
+                    </p>
+                  </a>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
