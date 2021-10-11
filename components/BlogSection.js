2453b3f8229f2187dbd763565873813954377e89
Jordan Duran <jordanduran@Jordans-MacBook-Pro.local>
2021-10-11 19:06:28 -0400

    Link to hashnode blog posts

--- components/BlogSection.js
+++ components/BlogSection.js
@@ -1,8 +1,8 @@
 const posts = [
   {
     title: 'Boost your conversion rate',
-    href: '#',
-    category: { name: 'Article', href: '#' },
+    href: 'https://hashnode.com/@jordanduran',
+    category: { name: 'Article', href: 'https://hashnode.com/@jordanduran' },
     description:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto accusantium praesentium eius, ut atque fuga culpa, similique sequi cum eos quis dolorum.',
     date: 'Mar 16, 2020',
@@ -12,15 +12,15 @@ const posts = [
     readingTime: '6 min',
     author: {
       name: 'Roel Aufderehar',
-      href: '#',
+      href: 'https://hashnode.com/@jordanduran',
       imageUrl:
         'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
     },
   },
   {
     title: 'How to use search engine optimization to drive sales',
-    href: '#',
-    category: { name: 'Article', href: '#' },
+    href: 'https://hashnode.com/@jordanduran',
+    category: { name: 'Article', href: 'https://hashnode.com/@jordanduran' },
     description:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit facilis asperiores porro quaerat doloribus, eveniet dolore. Adipisci tempora aut inventore optio animi., tempore temporibus quo laudantium.',
     date: 'Mar 10, 2020',
@@ -30,15 +30,15 @@ const posts = [
     readingTime: '4 min',
     author: {
       name: 'Brenna Goyette',
-      href: '#',
+      href: 'https://hashnode.com/@jordanduran',
       imageUrl:
         'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
     },
   },
   {
     title: 'Improve your customer experience',
-    href: '#',
-    category: { name: 'Article', href: '#' },
+    href: 'https://hashnode.com/@jordanduran',
+    category: { name: 'Article', href: 'https://hashnode.com/@jordanduran' },
     description:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint harum rerum voluptatem quo recusandae magni placeat saepe molestiae, sed excepturi cumque corporis perferendis hic.',
     date: 'Feb 12, 2020',
@@ -48,7 +48,7 @@ const posts = [
     readingTime: '11 min',
     author: {
       name: 'Daniela Metz',
-      href: '#',
+      href: 'https://hashnode.com/@jordanduran',
       imageUrl:
         'https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
     },
