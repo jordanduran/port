ebdda15b20f57832438023e5859a297f534682ef
Jordan Duran <jordanduran@Jordans-MacBook-Pro.local>
2021-10-13 16:05:54 -0400

    Added page title

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,27 @@
+import Head from 'next/head';
 import Hero from '../components/Hero';
 import ProjectsSection from '../components/ProjectsSection';
 import BlogSection from '../components/BlogSection';
 
 const HomePage = () => {
   return (
-    <div className='max-w-8xl px-4 py-8 sm:px-6 lg:px-8'>
-      <div className='hero-section max-w-6xl py-8 mx-auto'>
-        <Hero />
+    <>
+      <Head>
+        <title>Jordan Duran | Full-stack Developer</title>
+        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+      </Head>
+      <div className='max-w-8xl px-4 py-8 sm:px-6 lg:px-8'>
+        <div className='hero-section max-w-6xl py-8 mx-auto'>
+          <Hero />
+        </div>
+        <div className='project-section max-w-6xl py-8 mx-auto'>
+          <ProjectsSection />
+        </div>
+        <div className='project-section max-w-6xl py-8 mx-auto'>
+          <BlogSection />
+        </div>
       </div>
-      <div className='project-section max-w-6xl py-8 mx-auto'>
-        <ProjectsSection />
-      </div>
-      <div className='project-section max-w-6xl py-8 mx-auto'>
-        <BlogSection />
-      </div>
-    </div>
+    </>
   );
 };
 
